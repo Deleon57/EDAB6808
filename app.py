07b584c47491a37ddadfcf3940c56c1068350871
Deleon57 <2021368044@ufs4life.ac.za>
2025-03-19 11:50:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
 import matplotlib.pyplot as plt
 import numpy as np 
 import streamlit as st
+st.title("Test run")
+st.header("EDAB6808")
+st.subheader("This is a fun and interavtive module")           
+variable=st.button("Click me")
+if variable:
+# Generate random time series data
+  time_series = np.random.randn(100)
+  fig, ax = plt.subplots()
+  ax.plot(time_series)
+  ax.set_title("Random 100 time series")
+  ax.set_xlabel("Time")
+  ax.set_ylabel("Value")
+  st.pyplot(fig)
